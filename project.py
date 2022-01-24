872674e8ed39266edab2248acbb9a9aac8d56446
rafi <dg21rafia@study.davidgamecollege.com>
2022-01-24 13:37:24 +0000

    task1v1

--- project.py
+++ project.py
@@ -1,1 +1,12 @@
-print("hello world.")
+#task 1
+
+sentence = "ASK NOT WHAT YOUR COUNTRY CAN DO FOR YOU ASK WHAT YOU CAN DO FOR YOUR COUNTRY"
+sentence = sentence.split()
+word = "COUNTRY"
+positions = []
+
+for i in range(len(sentence)):
+    if sentence[i] == word:
+        positions.append(i+1)
+
+print(positions)

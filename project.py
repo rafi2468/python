3204effb3490d50a1b937a88d4f266364cfbbdfb
rafi <dg21rafia@study.davidgamecollege.com>
2022-01-24 13:42:49 +0000

    task1v2

--- project.py
+++ project.py
@@ -10,3 +10,14 @@ for i in range(len(sentence)):
         positions.append(i+1)
 
 print(positions)
+
+sentence = "ASK NOT WHAT YOUR COUNTRY CAN DO FOR YOU ASK WHAT YOU CAN DO FOR YOUR COUNTRY"
+sentence = sentence.split()
+word = "YOUR"
+positions = []
+
+for i in range(len(sentence)):
+    if sentence[i] == word:
+        positions.append(i+1)
+
+        print(positions)

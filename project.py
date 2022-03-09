048b5c01d28ab5665c1ddb0d3701d92774011ecd
rafi <98324920+rafi2468@users.noreply.github.com>
2022-03-09 11:44:11 +0000

    ex 3

--- project.py
+++ project.py
@@ -1,12 +1,34 @@
 def averageoflist():
-    sumOfNumbers = 10
     total = 0
-    for t in range(sumOfNumbers):
-        score = int(input("enter score "))
+    end = False
+    count = 0
+    names = []
+    scores = []
+    while end == False:
+        name = input("enter name: ")
+        score = int(input("enter score: "))
+        names.append(name)
+        scores.append(score)
         total += score
-
-    avg = total / sumOfNumbers
+        count += 1
+        cont = input("Do you want to add another score Y/N: ")
+        if cont == 'N':
+            end = True
+        highest = max(scores)
+        lowest = min(scores)
+        studentHighest = scores.index(highest)
+        studentLowest = scores.index(lowest)
+    avg = total / count
+    print(names[studentHighest], highest, names[studentLowest], lowest)
+    searched = int(input("Which score do you want to search for: "))
+    for i in range(len(scores)):
+        if searched == scores[i]:
+            print("Found", names[i], scores[i])
+            
+    
     return avg
+
+print(averageoflist())
 
 
 

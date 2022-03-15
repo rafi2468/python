54def96932cc81e58951191382e9776febef65f9
rafi <98324920+rafi2468@users.noreply.github.com>
2022-03-15 12:22:05 +0000

    guess.py

--- guess.py
+++ guess.py
@@ -1,13 +1,16 @@
-replacers = { "a": 0, "e":1, "i":2, "o":2, "u": 3 }  
-def encrypt(word):
+def test_result(word):
     word = word[::-1]
     return word
 
-def replace(word):  
+def replace(word): 
+    replacers = { "a": 0, "e":1, "i":2, "o":2, "u": 3 }      
     result = ''   
     for item in word:
         if item in replacers.keys():
-            result += replacers[item]
-
-print(encrypt("apple"))
+            result = result + str(replacers[item])
+        else:
+            result = result + item
+    return result + "aca"
  
+word = test_result("apple")
+print(replace(word))

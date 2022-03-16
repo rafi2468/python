9cfbd45b93d5969a92fabb70710fd24c85a45fd9
rafi <98324920+rafi2468@users.noreply.github.com>
2022-03-16 11:51:51 +0000

    python

--- guess.py
+++ guess.py
@@ -14,3 +14,21 @@ def replace(word):
  
 word = test_result("apple")
 print(replace(word))
+
+
+
+
+
+mylist = ["bob","ana","jim", "jak","tim"] 
+found = False
+searched = "henry"
+i = 0
+while found == False and i<len(mylist):
+    if mylist[i]== searched:
+         print ("found at" , i)
+         found = True
+    i  = i+1
+print("done")
+if found == False:
+    print("Item not found")    
+print("done")
---
+++ python/python 4.py
@@ -0,0 +1,11 @@
+def will_hit(equation, player_coordinaters):
+    character = equation[4]
+    if character == "-":
+        m= character + equation[5]
+
+    else:
+        character = character + equation[7]
+        m = character
+    charactor = equation[7]
+    if character == "-" 
+    

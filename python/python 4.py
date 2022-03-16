d5f1a66f54caf73d3d81342d81050e486510c97e
rafi <98324920+rafi2468@users.noreply.github.com>
2022-03-16 12:07:42 +0000

    python

--- python/python 4.py
+++ python/python 4.py
@@ -1,11 +1,22 @@
+from turtle import position
+
+
 def will_hit(equation, player_coordinaters):
     character = equation[4]
     if character == "-":
         m= character + equation[5]
+    else:
+        m = character
+        charactor = equation[7]
+    
+    if character == "-":
+        b= character + equation[-1]
+    else:
+        b= equation[-1]
+    
+    x,y = position
+    if (m*x + b == y):
+         return True
 
     else:
-        character = character + equation[7]
-        m = character
-    charactor = equation[7]
-    if character == "-" 
-    
+        return False

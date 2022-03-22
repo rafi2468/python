83ae12c69ff8b112ebd0c0e9c131e66622cb149a
rafi <98324920+rafi2468@users.noreply.github.com>
2022-03-22 11:24:07 +0000

    python

--- python/Untitled-2.py
+++ python/Untitled-2.py
@@ -1,7 +1,35 @@
 from tkinter import *
+is _on = True
 
+def clickhere():
+    global is_on 
+    if is_on:
+        l.config (text="Goodbye world")
+        is_on = False
+    else:
+        l.config(text="Hello world")
+        is_on = True
 
 window = Tk()
 
-Label(window, text= "Hello world").pack()
-Button(window, text= "Click here").pack()
+window.title("greeting")
+window.configure(background="megenta")
+window.geometry("450x450")
+window.realizable(True,False,)
+l = Label(window,text="Hello world")
+l.config(font = "impact", bg= "green", fg= "red")
+l.pack()
+
+b= Button (window, text = "Click here")
+b.config(font = "Magento", bg = "blue", fg = "yellow")
+b.pack()
+
+e = Entry(width=30)
+
+e.insert(0,"some text")
+e.pack()
+nameb = Button(window, text = "Enter name", command = clickhere)
+b.config(font = "Magento", bg = "blue", fg ="yellow")
+b.pack()
+
+window.mainloop()
